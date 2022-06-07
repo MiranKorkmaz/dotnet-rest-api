b035933102f92130f72e3de87a11868914b24e09
Miran Korkmaz <mirankorkmaz@outlook.com>
2022-06-07 12:31:06 +0200

    adding a controller file

--- data/MockCommanderRepo.cs
+++ data/MockCommanderRepo.cs
@@ -7,12 +7,17 @@ namespace Commander.data {
     {
         public IEnumerable<Command> GetAppCommands()
         {
-            throw new NotImplementedException();
+            var commands = new List<Command> {
+                new Command{Id=0, HowTo="Boil an egg", Line="Boil water", Platform="Kettle & Pan"},
+                new Command{Id=1, HowTo="Make a cup of tea", Line="Boil water", Platform="Kettle & Pan"},
+                new Command{Id=2, HowTo="Cut bread", Line="Get a knife", Platform="Knife and chopping board"}
+            };
+            return commands;
         }
 
         public Command GetCommandById(int id)
         {
-            throw new NotImplementedException();
+            return new Command{Id=0, HowTo="Boil an egg", Line="Boil water", Platform="Kettle & Pan"};
         }
     }
 }
